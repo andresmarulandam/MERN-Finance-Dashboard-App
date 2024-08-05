4bb21ef0ce59a76ce425c5aec911ad555a7f870e
Andres M <marulandaandres19@gmail.com>
2024-08-04 21:28:57 -0500

    Install recharts and create first chart (Revenue and Expenses)

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -4,54 +4,52 @@ import Row2 from './Row2';
 import Row3 from './Row3';
 
 const gridTemplateLargeScreens = `
-"a b c"
-"a b c"
-"a b c"
-"a b c"
-"d e f"
-"d e f"
-"d h i"
-"g h i"
-"g h j"
-"g h j"
+  "a b c"
+  "a b c"
+  "a b c"
+  "a b f"
+  "d e f"
+  "d e f"
+  "d h i"
+  "g h i"
+  "g h j"
+  "g h j"
 `;
-
-const gridTemplateSmallScrens = `
-"a"
-"a"
-"a"
-"a"
-"b"
-"b"
-"b"
-"b"
-"c"
-"c"
-"c"
-"d"
-"d"
-"d"
-"d"
-"e"
-"e"
-"f"
-"f"
-"g"
-"g"
-"g"
-"h"
-"h"
-"h"
-"h"
-"i"
-"i"
-"j"
-"j"
+const gridTemplateSmallScreens = `
+  "a"
+  "a"
+  "a"
+  "a"
+  "b"
+  "b"
+  "b"
+  "b"
+  "c"
+  "c"
+  "c"
+  "d"
+  "d"
+  "d"
+  "e"
+  "e"
+  "f"
+  "f"
+  "f"
+  "g"
+  "g"
+  "g"
+  "h"
+  "h"
+  "h"
+  "h"
+  "i"
+  "i"
+  "j"
+  "j"
 `;
 
 const Dashboard = () => {
-  const isAboveMediumScreens = useMediaQuery('(min-width:1200px)');
-
+  const isAboveMediumScreens = useMediaQuery('(min-width: 1200px)');
   return (
     <Box
       width="100%"
@@ -61,14 +59,14 @@ const Dashboard = () => {
       sx={
         isAboveMediumScreens
           ? {
-              gridTemplateColumns: 'repeat( 3 , minmax( 370px, 1fr))',
-              gridTemplateRows: ' repeat ( 10, minmax(60px, 1fr))',
+              gridTemplateColumns: 'repeat(3, minmax(370px, 1fr))',
+              gridTemplateRows: 'repeat(10, minmax(60px, 1fr))',
               gridTemplateAreas: gridTemplateLargeScreens,
             }
           : {
               gridAutoColumns: '1fr',
-              gridAutoRows: '80px ',
-              gridTemplateAreas: gridTemplateSmallScrens,
+              gridAutoRows: '80px',
+              gridTemplateAreas: gridTemplateSmallScreens,
             }
       }
     >
