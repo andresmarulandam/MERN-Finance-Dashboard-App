4b1aa24243d210fd5d89fbbe82b3f30b12b7b0c7
Andres M <marulandaandres19@gmail.com>
2024-08-11 13:59:12 -0500

    Add DataGrid List of Products

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,11 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import kpiRoutes from './routes/kpi.js';
 import productRoutes from './routes/product.js';
+import transactionRoutes from './routes/transaction.js';
 import KPI from './model/KPI.js';
 import Product from './model/Product.js';
-import { kpis, products } from './data/data.js';
+import Transaction from './model/Transaction.js';
+import { kpis, products, transactions } from './data/data.js';
 
 /*Paso 5 CONFIGURATIONS*/
 dotenv.config();
@@ -25,6 +27,7 @@ app.use(cors());
 /*Paso 9. ROUTES*/
 app.use('/kpi', kpiRoutes);
 app.use('/product', productRoutes);
+app.use('/transactions', transactionRoutes);
 
 /* Paso 7. */
 const PORT = process.env.PORT || 9000;
@@ -34,8 +37,11 @@ mongoose
   .then(async () => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-    await mongoose.connection.db.dropDatabase();
-    KPI.insertMany(kpis);
-    Product.insertMany(products);
+    /* ADD DATA ONE TIME ONLY OR AS NEEDED */
+
+    // await mongoose.connection.db.dropDatabase();
+    // KPI.insertMany(kpis);
+    // Product.insertMany(products);
+    // Transaction.insertMany(transactions);
   })
   .catch((error) => console.log(`${error} did not connect`));
